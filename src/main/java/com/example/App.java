0b2e19668a28b8e454720f58b92741a966e4fbee
lai2-24 <lai2-24@itismeucci.com>
2023-11-06 10:59:03 +0100

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,54 @@
+package com.example;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        try {
+            Socket s = new Socket("localhost", 6790);
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+            BufferedReader inServ = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            String st="";
+            String nota = "";
+            int a = 0;
+       
+            do {
+                
+                System.out.println(inServ.readLine());
+                st = in.readLine();
+                out.writeBytes(st + '\n');
+                
+                if (st.equals("Aggiungi nota"))
+                {
+                    System.out.println("Aggiungi nota");
+                   
+                    nota = in.readLine();
+                    out.writeBytes(st + "\n");
+                    System.out.println(inServ.readLine());
+                    
+                }
+                else if (st.equals("Uscita"))
+                {
+                    a=1;
+                    
+                }
+                else{
+                    System.out.println(inServ.readLine());
+                }
+                
+                
+            } while (a == 0);
+            s.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("errore durante la comunicazione!");
+            System.exit(1);
+        }
+    }
+}
